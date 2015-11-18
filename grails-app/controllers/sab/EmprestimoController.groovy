4c32f9692264854e29d7e9c90b9ac3690be313a3
Gean Saturno Gonçalves <cronosnaeg@gmail.com>
2015-11-18 16:20:32 -0200

    Generate all de Emprestimo

---
+++ grails-app/controllers/sab/EmprestimoController.groovy
@@ -0,0 +1,107 @@
+package sab
+
+import static org.springframework.http.HttpStatus.*
+import grails.transaction.Transactional
+
+@Transactional(readOnly = true)
+class EmprestimoController {
+
+    static allowedMethods = [save: "POST", update: "PUT", delete: "DELETE"]
+
+    def index(Integer max) {
+        params.max = Math.min(max ?: 10, 100)
+        respond Emprestimo.list(params), model:[emprestimoCount: Emprestimo.count()]
+    }
+
+    def show(Emprestimo emprestimo) {
+        respond emprestimo
+    }
+
+    def create() {
+        respond new Emprestimo(params)
+    }
+
+    @Transactional
+    def save(Emprestimo emprestimo) {
+        if (emprestimo == null) {
+            transactionStatus.setRollbackOnly()
+            notFound()
+            return
+        }
+
+        if (emprestimo.hasErrors()) {
+            transactionStatus.setRollbackOnly()
+            respond emprestimo.errors, view:'create'
+            return
+        }
+
+        emprestimo.save flush:true
+
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.created.message', args: [message(code: 'emprestimo.label', default: 'Emprestimo'), emprestimo.id])
+                redirect emprestimo
+            }
+            '*' { respond emprestimo, [status: CREATED] }
+        }
+    }
+
+    def edit(Emprestimo emprestimo) {
+        respond emprestimo
+    }
+
+    @Transactional
+    def update(Emprestimo emprestimo) {
+        if (emprestimo == null) {
+            transactionStatus.setRollbackOnly()
+            notFound()
+            return
+        }
+
+        if (emprestimo.hasErrors()) {
+            transactionStatus.setRollbackOnly()
+            respond emprestimo.errors, view:'edit'
+            return
+        }
+
+        emprestimo.save flush:true
+
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.updated.message', args: [message(code: 'emprestimo.label', default: 'Emprestimo'), emprestimo.id])
+                redirect emprestimo
+            }
+            '*'{ respond emprestimo, [status: OK] }
+        }
+    }
+
+    @Transactional
+    def delete(Emprestimo emprestimo) {
+
+        if (emprestimo == null) {
+            transactionStatus.setRollbackOnly()
+            notFound()
+            return
+        }
+
+        emprestimo.delete flush:true
+
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.deleted.message', args: [message(code: 'emprestimo.label', default: 'Emprestimo'), emprestimo.id])
+                redirect action:"index", method:"GET"
+            }
+            '*'{ render status: NO_CONTENT }
+        }
+    }
+
+    protected void notFound() {
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.not.found.message', args: [message(code: 'emprestimo.label', default: 'Emprestimo'), params.id])
+                redirect action: "index", method: "GET"
+            }
+            '*'{ render status: NOT_FOUND }
+        }
+    }
+}
---
+++ grails-app/views/emprestimo/create.gsp
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta name="layout" content="main" />
+        <g:set var="entityName" value="${message(code: 'emprestimo.label', default: 'Emprestimo')}" />
+        <title><g:message code="default.create.label" args="[entityName]" /></title>
+        <asset:javascript src="jquery-ui.js"/>
+        <asset:stylesheet src="jquery-ui.css"/>
+
+        <g:javascript>
+            $.get("${createLink(controller: 'Pessoa', action: 'listagem')}", function(pessoas){
+                $('#pessoas').autocomplete({source: pessoas});
+            });
+        </g:javascript>
+    </head>
+    <body>
+        <a href="#create-emprestimo" class="skip" tabindex="-1"><g:message code="default.link.skip.label" default="Skip to content&hellip;"/></a>
+        <div class="nav" role="navigation">
+            <ul>
+                <li><a class="home" href="${createLink(uri: '/')}"><g:message code="default.home.label"/></a></li>
+                <li><g:link class="list" action="index"><g:message code="default.list.label" args="[entityName]" /></g:link></li>
+            </ul>
+        </div>
+        <div id="create-emprestimo" class="content scaffold-create" role="main">
+            <h1><g:message code="default.create.label" args="[entityName]" /></h1>
+            <g:if test="${flash.message}">
+            <div class="message" role="status">${flash.message}</div>
+            </g:if>
+            <g:hasErrors bean="${this.emprestimo}">
+            <ul class="errors" role="alert">
+                <g:eachError bean="${this.emprestimo}" var="error">
+                <li <g:if test="${error in org.springframework.validation.FieldError}">data-field-id="${error.field}"</g:if>><g:message error="${error}"/></li>
+                </g:eachError>
+            </ul>
+            </g:hasErrors>
+            <g:form action="save">
+                <fieldset class="form">
+                    <f:field bean="emprestimo" property="pessoa">
+                        <input id="pessoas" autofocus name="pessoa"/>
+                    </f:field>
+
+                    <f:all bean="emprestimo" except="dataRealDeDevolucao, pessoa, livros"/>
+                </fieldset>
+                <fieldset class="buttons">
+                    <g:submitButton name="create" class="save" value="${message(code: 'default.button.create.label', default: 'Create')}" />
+                </fieldset>
+            </g:form>
+        </div>
+    </body>
+</html>
