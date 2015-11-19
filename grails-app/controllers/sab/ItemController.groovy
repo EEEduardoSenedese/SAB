b264edfa505584286f0049c3807ff45a46835e3c
Gean Saturno Gonçalves <cronosnaeg@gmail.com>
2015-11-19 09:11:37 -0200

    Controlador de Item

---
+++ grails-app/controllers/sab/ItemController.groovy
@@ -0,0 +1,107 @@
+package sab
+
+import static org.springframework.http.HttpStatus.*
+import grails.transaction.Transactional
+
+@Transactional(readOnly = true)
+class ItemController {
+
+    static allowedMethods = [save: "POST", update: "PUT", delete: "DELETE"]
+
+    def index(Integer max) {
+        params.max = Math.min(max ?: 10, 100)
+        respond Item.list(params), model:[itemCount: Item.count()]
+    }
+
+    def show(Item item) {
+        respond item
+    }
+
+    def create() {
+        respond new Item(params)
+    }
+
+    @Transactional
+    def save(Item item) {
+        if (item == null) {
+            transactionStatus.setRollbackOnly()
+            notFound()
+            return
+        }
+
+        if (item.hasErrors()) {
+            transactionStatus.setRollbackOnly()
+            respond item.errors, view:'create'
+            return
+        }
+
+        item.save flush:true
+
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.created.message', args: [message(code: 'item.label', default: 'Item'), item.id])
+                redirect item
+            }
+            '*' { respond item, [status: CREATED] }
+        }
+    }
+
+    def edit(Item item) {
+        respond item
+    }
+
+    @Transactional
+    def update(Item item) {
+        if (item == null) {
+            transactionStatus.setRollbackOnly()
+            notFound()
+            return
+        }
+
+        if (item.hasErrors()) {
+            transactionStatus.setRollbackOnly()
+            respond item.errors, view:'edit'
+            return
+        }
+
+        item.save flush:true
+
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.updated.message', args: [message(code: 'item.label', default: 'Item'), item.id])
+                redirect item
+            }
+            '*'{ respond item, [status: OK] }
+        }
+    }
+
+    @Transactional
+    def delete(Item item) {
+
+        if (item == null) {
+            transactionStatus.setRollbackOnly()
+            notFound()
+            return
+        }
+
+        item.delete flush:true
+
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.deleted.message', args: [message(code: 'item.label', default: 'Item'), item.id])
+                redirect action:"index", method:"GET"
+            }
+            '*'{ render status: NO_CONTENT }
+        }
+    }
+
+    protected void notFound() {
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.not.found.message', args: [message(code: 'item.label', default: 'Item'), params.id])
+                redirect action: "index", method: "GET"
+            }
+            '*'{ render status: NOT_FOUND }
+        }
+    }
+}
