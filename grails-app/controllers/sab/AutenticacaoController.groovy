cc951a69956c2d4ea0c228c103b80f9f7a3e0afa
Gean Saturno Gonçalves <cronosnaeg@gmail.com>
2015-12-11 09:55:18 -0200

    Correção de bugs no momento do redirecionamento

--- grails-app/controllers/sab/AutenticacaoController.groovy
+++ grails-app/controllers/sab/AutenticacaoController.groovy
@@ -4,13 +4,15 @@ import sab.individuo.Pessoa
 class AutenticacaoController {
 
     def index(){
+        if (session['usuario']) {
+            redirect action: 'bemvindo'
+        }
     }
 
     def autenticar(){
         def pessoa = Pessoa.findByUsuarioAndSenha(params.usuario, params.senha)
         if (pessoa){
             session['usuario'] = pessoa
-
             redirect (action: "bemvindo")
         } else{
             flash.message = "Acesso Negado"
