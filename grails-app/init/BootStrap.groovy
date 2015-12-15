24ab94460464b274c60b8552de787e0032d9b02e
Gean Saturno Gonçalves <cronosnaeg@gmail.com>
2015-12-15 12:04:48 -0200

    Administrador pode ser criado do 0

--- grails-app/init/BootStrap.groovy
+++ grails-app/init/BootStrap.groovy
@@ -1,22 +1,30 @@
 import sab.endereco.*
 import sab.individuo.*
+import sab.livros.*
 
 class BootStrap {
 
     def init = { servletContext ->
+
         def pessoa = Pessoa.findByUsuario("admin")
 
         if(!pessoa){
+            //Endereço
+            UF uf = UF.findOrSaveByNome('Não Informado')
+            Cidade cidade = Cidade.findOrSaveByNomeAndUf('Não Informada', uf)
+            Bairro bairro = Bairro.findOrSaveByNomeAndCidade("Não Informado", cidade)
+            Rua rua = Rua.findOrSaveByNomeAndBairro("Não Informada", bairro)
+
             pessoa = new Pessoa(
                 nome: "Administrador",
                 usuario: "admin",
                 senha: "admin",
-                bairro: Bairro.list(max: 1)[0],
-                cidade: Cidade.list(max: 1)[0],
-                posicao: Posicao.list(max: 1)[0],
-                uf: UF.list(max: 1)[0],
-                rua: Rua.list(max: 1)[0],
-                sexo: Sexo.list(max:1)[0],
+                uf: uf,
+                cidade: cidade,
+                bairro: bairro,
+                rua: rua,
+                sexo: Sexo.findOrSaveByNome('Não Informado'),
+                posicao: Posicao.findOrSaveByNome('Administrador'),
                 numeroDaRua: 0,
                 dataDeNascimento: new Date(),
                 numeroDeEmprestimos: 0,
